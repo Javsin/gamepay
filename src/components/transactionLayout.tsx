d07861ad9724eee33f4e4212627cde81cfe3acda
javsin <anaswidayanto@gmail.com>
2024-04-29 14:13:02 +0700

    push

--- src/components/transactionLayout.tsx
+++ src/components/transactionLayout.tsx
@@ -7,6 +7,9 @@ import checkProduct from "@/action/transaction/checkProduct"
 import { Pembelian } from "@/types/buyProduct";
 import { useState } from "react";
 import Modal from "@/components/modal";
+import Link from "next/link";
+import IconCheck from "@/public/assets/icon_cek_transaksi.png";
+import Image from "next/image";
 type TransactionLayoutProps = {
     description : React.ReactNode;
     testimoni? : React.ReactNode;
@@ -33,6 +36,7 @@ const transactionLayout = ({description,testimoni,form,product,quantity,paymentM
     const contactValue = context?.contact;
     const isCekValue = context?.isCek;
     const elementAccountRef = context?.elementAccountRef;
+    const setLoadingCheck = context?.setLoadingCheck ?? (() => {});
     const order = async (e : React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const formElements = e.currentTarget.elements;
@@ -68,10 +72,15 @@ const transactionLayout = ({description,testimoni,form,product,quantity,paymentM
             toast.error("Harap masukkan kontak anda.")
         }else{
             if(isCekValue){
+                setLoadingCheck(true);
                 const responseCheck = await checkProduct(formData as Pembelian);
                 if(responseCheck.status === 200){
                     setValueCheck(responseCheck.data);
                     toggle();
+                    setLoadingCheck(false);
+                }else{
+                    setLoadingCheck(false);
+                    toast.error("Terjadi kesalahan, silahkan coba lagi.")
                 }
             }
         }
@@ -130,30 +139,39 @@ const transactionLayout = ({description,testimoni,form,product,quantity,paymentM
                 pauseOnHover
                 theme="dark"
             />
-            <Modal isOpen={isOpen} closeModal={toggle} title='' size='w-[35rem]' showClose={false} background='bg-[#313E75] rounded-xl text-white'>
+            <Modal isOpen={isOpen} closeModal={toggle} title='' size='w-[33rem]' showClose={false} background='bg-[#313E75] rounded-xl text-white'>
                 <div className="border-b border-[#4E66D9] flex items-center gap-3">
-                    <div className="bg-orange-500 py-2">
-                        icon
+                    <div className="bg-orange-500 p-3 h-full">
+                        <Image src={IconCheck} alt="icon-check" width={0} height={0} sizes="100vw" className="w-6 h-auto" />
                     </div>
-                    <div className="py-2">
-                        ww
+                    <div className="py-2 font-semibold">
+                        Buat Pesanan
                     </div>
                 </div>
-                <div className="p-6 grid grid-cols-2">
+                <div className="pt-6 py-4 px-6 grid grid-cols-1">
+                    <div className="py-3 xl:px-10 text-center bg-[#e53e60] rounded-xl text-white font-light text-sm xl:text-base">
+                        Pastikan data akun dan produk yang kamu pilih telah valid dan sesuai
+                    </div>
+                </div>
+                <div className="py-4 px-6 grid grid-cols-2">
                     {
                         valueCheck.map((item : any, index) => {
                             return (
-                                <div className="col-span-1 gap-2" key={index}>
+                                <div className="col-span-1 gap-4 mb-6" key={index}>
                                     <div className="text-sm font-light">
                                         {item.key}
                                     </div>
-                                    <div>
+                                    <div className="font-medium">
                                         {item.value}
                                     </div>
                                 </div>
                             )
                         })
                     }
+                </div>
+                <div className="pt-4 pb-10 px-6 xl:flex xl:flex-row xl:gap-3 grid grid-cols-1 xl:text-base text-sm">
+                    <a href="#" className="w-full py-2 bg-[#556EB1] text-white rounded-lg text-center xl:my-0 my-2" onClick={()=>toggle()}>Batalkan</a>
+                    <Link href="#" className="w-full py-2 bg-orange-500 text-white rounded-lg text-center order-first xl:order-2">Pesan Sekarang</Link>
                 </div>
             </Modal>
         </>
