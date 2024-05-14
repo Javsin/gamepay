1a561d97ab53b21b4cc901c77e19569bc2b490d1
gianputra13 <gianputra131@gmail.com>
2024-05-14 11:16:30 +0700

    add file ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,13 @@ module.exports = {
     apps: [
         {
             name: "MURAGAME_NEXTJS",
-            script: "npm run start-prod",
+            script: "node_modules/next/dist/bin/next",
+            args: "start",
             env: {
             NODE_ENV: "production",
             },
             watch: true,
-            max_memory_restart: '300M'
+            max_memory_restart: '1G'
         },
     ],
 };
