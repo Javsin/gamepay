1c235a98c16912005d31ea7a629ee21ca9cffe96
gianputra13 <gianputra131@gmail.com>
2024-06-10 14:39:52 +0700

    hidden form qty

--- src/app/transaction/[productId]/@promo/page.tsx
+++ src/app/transaction/[productId]/@promo/page.tsx
@@ -62,7 +62,7 @@ export default function Promo() {
             <div className="bg bg-dark-blue-2 rounded-xl">
                 <div className="flex gap-2 text-white items-center border-b border-[#374585]">
                     <div className="bg-orange bg-orange-500 rounded-tl-xl py-2 px-4">
-                        5
+                        4
                     </div>
                     <div>
                         Kode Promo
