1d9777de2b85106744b64a85a63df88bfff55c3a
gianputra13 <gianputra131@gmail.com>
2024-06-15 08:50:17 +0700

    add Android cetak

--- src/components/invoices/buttonDownloadInvoice.tsx
+++ src/components/invoices/buttonDownloadInvoice.tsx
@@ -2,12 +2,35 @@
 
 import generatePDF from '@/components/invoices/invoicePDF';
 import { InvoiceData } from '@/types/invoiceType';
+import { useEffect } from 'react';
 
 interface downloadInvoiceProp {
     data: InvoiceData;
 }
 
 const buttonDownloadInvoice = ({data} : downloadInvoiceProp) => {
+
+    const {payment_methode, product_name} = data;
+    const {qris_data, total_tagihan, tagihan, exp, tujuan} = data.payment_detail;
+
+    useEffect(() => {
+        try{
+            // @ts-ignore
+            Android.cetak(JSON.stringify({
+                qrcode: qris_data,
+                paymentMethod: payment_methode,
+                adminQris: total_tagihan - tagihan,
+                total_tagihan: total_tagihan,
+                harga_produk: tagihan,
+                tgl_expired: exp,
+                tujuan: tujuan,
+                produk: product_name
+            }))
+        }catch(error){
+            console.log(error)
+        }
+    })
+
     const handleDownloadInvoice = () => {
         generatePDF(data);
     };
